d7353e4fd72027b6451860f9b1e75a8506fb38af
AleCuzea28 <gabriela.cuzea@student.upt.ro>
2023-10-30 12:47:39 +0200

    Error Page and Register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <div>
-      return <h1>Login</h1>
+      <h1>Login</h1>
       <Link to="/register">Register Page</Link>
     </div>
   );
